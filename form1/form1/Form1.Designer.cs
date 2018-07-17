d8e0dbfba9a5a6f8b11f0b1d0eb40736126733f2
PoojaBaddure <40929603+PoojaBaddure@users.noreply.github.com>
2018-07-17 15:30:15 +0530

    this is first dot net form

---
+++ form1/form1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "user name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 159);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "password";
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(339, 59);
+            this.t1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(304, 22);
+            this.t1.TabIndex = 2;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(339, 159);
+            this.t2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(304, 22);
+            this.t2.TabIndex = 3;
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(183, 292);
+            this.b1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(123, 28);
+            this.b1.TabIndex = 4;
+            this.b1.Text = "login";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(450, 292);
+            this.b2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(112, 28);
+            this.b2.TabIndex = 5;
+            this.b2.Text = "cancel";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 417);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+    }
+}
+
---
+++ form1/form1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Hp\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            SqlDataAdapter sda = new SqlDataAdapter("select Count(*) from Login where Username='" + t1.Text +"' and password='" + t2.Text + "'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                main ss = new main();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("please check your username and password");
+            }
+        }
+          
+    }
+}
